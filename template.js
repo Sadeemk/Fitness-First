287c95f3097badc757fd9d8b883e53c6f3375c3d
Richard Pena <penarichard53@gmail.com>
2018-11-08 21:17:17 -0800

    adding local json var

--- template.js
+++ template.js
@@ -1,11 +1,195 @@
+var json = [
+  {
+    "name": "Chest",
+    "description": "The workout here follows a reverse-pyramid protocol, which allows you to take more total sets to muscle failure. It also starts with the dumbbell bench press rather than the barbell, because dumbbells are more difficult to control—extra difficulty is a good thing in this case—and allow for a greater range of motion. Many people find that just making that switch is enough to spur new growth.",
+    "notes": [
+        {"note": "Choose a weight that allows you to reach muscle failure by the target rep listed."},
+        {"note":"Lighten the weight for slightly higher reps on each set after your first 1-2 sets of each movement."},
+        {"note":"If you have a spotter, do a few forced reps on your heaviest set of each exercise, which should be your first or second set. If you don't have a partner, train as close to muscle failure as possible, and perform a dropset on your last set of each exercise."}
+    ],
+    "exercises": {
+      "exercise-1": {
+          "name": "Dumbbell Bench Press",
+          "sets": "4 sets x 6-8 reps",
+          "video": "https://www.youtube.com/embed/gRVjAtPip0Y"
+      },
+      "exercise-2": {
+          "name": "Barbell Incline Bench Press Medium-Grip",
+          "sets": "3 sets x 8-10 reps",
+          "video": "https://www.youtube.com/embed/SrqOu55lrYU"
+      },
+      "exercise-3": {
+          "name": "Leverage Decline Chest Press",
+          "sets": "3 sets x 8-10 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      },
+      "exercise-4": {
+          "name": "Incline Dumbbell Flyes",
+          "sets": "3 sets x 8-10 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      },
+      "exercise-5": {
+          "name": "Butterfly",
+          "sets": "3 sets x 12 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      }
+    }
+  },
+  {
+    "name": "Shoulders",
+    "description": "Because the front delts get so much work on chest day, and the middle delts take the brunt of the weight on overhead pressing moves, it's not uncommon for the rear delts to be the smallest—and weakest—of the three. In this workout, you'll do them before the other two, while you have a bit more energy in the tank. Feel free to rearrange the order of the single-joint moves based on your weaknesses, though. If you judge your delts to be fairly balanced, simply rotate the order of the single-joint movements from one workout to the next.",
+    "notes": [
+        {"note": "These workouts don't include warm-up sets. Do as many as you need, but never take warm-ups to muscle failure."},
+        {"note":"Choose a weight that allows you to reach muscle failure by the target-rep listed"},
+        {"note":"If you have a spotter, do a few forced reps on your heaviest sets of overhead presses. If you don't have a partner, do a dropset on your last set of each exercise, reducing the weight by about 25 percent when you reach muscle failure and continuing on to a second point of muscle failure."}
+    ],
+    "exercises": {
+      "exercise-1": {
+          "name": "Dumbbell Shoulder Press",
+          "sets": "4 sets x 6-8 reps",
+          "video": "https://www.youtube.com/embed/gRVjAtPip0Y"
+      },
+      "exercise-2": {
+          "name": "Upright Barbell Row",
+          "sets": "3 sets x 8-10 reps",
+          "video": "https://www.youtube.com/embed/SrqOu55lrYU"
+      },
+      "exercise-3": {
+          "name": "Seated Bent-Over Rear Delt Raise",
+          "sets": "3 sets x 8-10 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      },
+      "exercise-4": {
+          "name": "Side Lateral Raise",
+          "sets": "3 sets x 10-12 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      },
+      "exercise-5": {
+          "name": "Standing Front Barbell Raise Over Head",
+          "sets": "3 sets x 12 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      }
+    }
+  },
+  {
+    "name": "Back",
+    "description": "In most cases the back is an afterthought for many beginners. The back can't be seen when you look in the mirror, and most people have a hard time contracting the back when they first start working out, so the majority of beginners do a few half-hearted sets of pull-downs and low cable rows and move on to something else.",
+    "notes": [
+        {"note": "Use a thumb press grip, where you use an over hand grip with your 4 fingers around the bar and press your thumbs on the bar. This will lead into the next pointer."},
+        {"note":"Use enough pressure on the bar to hold, but don't squeeze too hard on the bar or you will engage the forearms and biceps too much. The thumb press will help with this."},
+        {"note":"Holding onto the bar or weight, drive you elbows as far back as possible trying to pinch your shoulder blades together."},
+        {"note": "Feel your muscles contract and leave your ego at the door. Too many beginners use way to much weight and never feel the muscle working."}
+    ],
+    "exercises": {
+      "exercise-1": {
+          "name": "Bent Over Barbell Row",
+          "sets": "3 sets x 6-8 reps",
+          "video": "https://www.youtube.com/embed/gRVjAtPip0Y"
+      },
+      "exercise-2": {
+          "name": "Seated Cable Rows",
+          "sets": "3 sets x 10-12 reps",
+          "video": "https://www.youtube.com/embed/SrqOu55lrYU"
+      },
+      "exercise-3": {
+          "name": "V-Bar Pulldown",
+          "sets": "3 sets x 8-10 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      },
+      "exercise-4": {
+          "name": "Barbell Shrug",
+          "sets": "3 sets x 6-8 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      },
+      "exercise-5": {
+          "name": "Hyperextensions (Back Extensions)",
+          "sets": "3 sets x 12 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      }
+    }
+  },
+  {
+    "name": "Legs",
+    "description": "As you can see from the anatomy info above, there are a lot of muscles that make up the legs. The legs are used daily for basic activities like walking, standing, going upstairs, or just getting up from a chair. What does this mean to you as a trainer? If you are looking to put size on your legs, half ass workouts will not do anything; your legs are accustomed to working all day. Only dedication to intense hard work in the gym will break down the dense thick muscle fibers of the legs and their only choice will be to get bigger and stronger. You need to force your legs to grow. Your legs will resist you. They will burn, shake and burn some more while you work out, but you need to push through and want more, as this is the only way to make your legs grow.",
+    "notes": [
+        {"note": "With the isolation exercises I like to use a moderate (8-12) to high rep range (15-20). This helps pump up the muscle to deliver more nutrients and break up the fascia tissue for more growth."},
+        {"note":"All exercises should be performed in perfect form because bad form or habits you start now will follow you and will lead to lack of progress—or worse, an injury in the future."},
+        {"note":"Now that you understand which muscles make up your legs, their function, location, how to contract your back and the rep range needed to stimulate it, let's give you some workouts to help you build your legs."}
+    ],
+    "exercises": {
+      "exercise-1": {
+          "name": "Barbell Squat",
+          "sets": "4 sets x 6-8 reps",
+          "video": "https://www.youtube.com/embed/gRVjAtPip0Y"
+      },
+      "exercise-2": {
+          "name": "Dumbbell Lunges",
+          "sets": "4 sets x 12 reps",
+          "video": "https://www.youtube.com/embed/SrqOu55lrYU"
+      },
+      "exercise-3": {
+          "name": "Leg Press",
+          "sets": "3 sets x 8-10 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      },
+      "exercise-4": {
+          "name": "Lying Leg Curls",
+          "sets": "3 sets x 12 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      },
+      "exercise-5": {
+          "name": "Leg Extensions",
+          "sets": "3 sets x 20 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      },
+      "exercise-6": {
+          "name": "Standing Calf Raises",
+          "sets": "4 sets x 12 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      }
+    }
+  },
+  {
+    "name": "Arms",
+    "description": "Measure yours—flexed, of course!—with a tape at the peak before and after a few weeks under this protocol. The supersets will leave you gassed, so don't expect to move much weight in the skullcrushers and close-grip bench at the end! This routine is meant to plug into your existing workout using the classic \"bis with back, tris with chest\" split. You use your biceps heavily in pulling lifts like pull-ups, rows, and even deadlifts, while your triceps play a major role in all manner of push motions, from the bench to the military press.",
+    "notes": [
+        {"note": "Start with your big-ticket strength movements, then chase a pump to your heart's content. Take both equally seriously, and you'll see improvement in both your core lifts and your mirror muscles!"}
+    ],
+    "exercises": {
+      "exercise-1": {
+          "name": "Incline Dumbbell Curl",
+          "sets": "4 sets x 10 reps",
+          "video": "https://www.youtube.com/embed/gRVjAtPip0Y"
+      },
+      "exercise-2": {
+          "name": "Hammer Curls",
+          "sets": "4 sets x 10 reps",
+          "video": "https://www.youtube.com/embed/SrqOu55lrYU"
+      },
+      "exercise-3": {
+          "name": "Close-Grip EZ Bar Curl",
+          "sets": "4 sets x 10 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      },
+      "exercise-4": {
+          "name": "EZ-Bar Curl",
+          "sets": "5 sets x 12, 10, 8, 8, 6 reps",
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
+      }
+    }
+  }
+]
+
 var json;
 var queryParams = new URLSearchParams(window.location.search);
 var projectTitle = queryParams.get('project');
 console.log('query for', projectTitle);
 
+/* github JSON call not working
 $.getJSON("http://penarichard53.github.io/fitness-first/exercise.json", function(data) {
   json = data;
 });
+*/
 
 $.getJSON("./exercise.json", function(data) {
   if(json == undefined){
@@ -15,7 +199,6 @@ $.getJSON("./exercise.json", function(data) {
 
 $(document).ready(function () {
   console.log(json);
-
 
   var source    = $('#entry-template').html();
   var template  = Handlebars.compile(source);
@@ -29,6 +212,6 @@ $(document).ready(function () {
       var curHtml = template(curData);
       parentDiv.append(curHtml);
     }
-    console.log(curData);
+    // console.log(curData);
   }
 });
