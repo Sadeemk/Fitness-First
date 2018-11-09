072619ea81fe3b804a6cd4616745c144e8b2eb77
GoslingtheFlighty <GoslingtheFlighty@outlook.com>
2018-11-09 01:38:15 -0800

    Made Homepage Styled

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-// testing git pull
-=======
-<<<<<<< HEAD
-/* Christie was here. */
-=======
-// testing git pulling
->>>>>>> 0b0da6a1b424ce56f1ad6509bc46a6b1ab152ddb
-// hello Christie
-
-
->>>>>>> c5dc41befedac84b9d56feb0f1460ee12fa3acfc
-function goBack() {
-    window.history.back();
-}
-
-function goBackRoutine() {
-    window.location = "./routines.html";
-}
 
 function myFunction(e) {
   var dropdown = e.parentNode.getElementsByClassName('dropdown-content')[0];
