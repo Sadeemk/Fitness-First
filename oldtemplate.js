868194ce6f96a5160c100de7ef9e819f5be8037e
GoslingtheFlighty <GoslingtheFlighty@outlook.com>
2018-11-27 01:53:38 -0800

    Updated tips in redesign

--- oldtemplate.js
+++ oldtemplate.js
@@ -4,16 +4,17 @@ var json = [
   {
     "name": "Chest",
     "description": "The workout here follows a reverse-pyramid protocol, which allows you to take more total sets to muscle failure. It also starts with the dumbbell bench press rather than the barbell, because dumbbells are more difficult to control—extra difficulty is a good thing in this case—and allow for a greater range of motion. Many people find that just making that switch is enough to spur new growth.",
-
+    "notes": [
+        {"note": "Choose a weight that allows you to reach muscle failure by the target rep listed."},
+        {"note":"Lighten the weight for slightly higher reps on each set after your first 1-2 sets of each movement."},
+        {"note":"If you have a spotter, do a few forced reps on your heaviest set of each exercise, which should be your first or second set. If you don't have a partner, train as close to muscle failure as possible, and perform a dropset on your last set of each exercise."}
+    ],
     "exercises": [
       {"exercise": {
           "names": "Dumbbell Bench Press",
           "sets": "4 sets x 6-8 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-1.gif",
           "video": "https://www.youtube.com/embed/gRVjAtPip0Y",
-          "notes": [
-              {"note":"If you have a spotter, do a few forced reps on your heaviest set of each exercise, which should be your first or second set. If you don't have a partner, train as close to muscle failure as possible, and perform a dropset on your last set of each exercise."}
-          ],
         },
       },
       {"exercise": {
@@ -27,10 +28,7 @@ var json = [
           "names": "Leverage Decline Chest Press",
           "sets": "3 sets x 8-10 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-1.gif",
-          "video": "https://www.youtube.com/embed/xK9zpXvjFUg",
-          "notes": [
-              {"note":"Lighten the weight for slightly higher reps on each set after your first 1-2 sets of each movement."},
-          ],
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
         },
       },
       {"exercise": {
@@ -44,10 +42,7 @@ var json = [
           "names": "Butterfly",
           "sets": "3 sets x 12 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-1.gif",
-          "video": "https://www.youtube.com/embed/xK9zpXvjFUg",
-          "notes": [
-              {"note":"If you have a spotter, do a few forced reps on your heaviest set of each exercise, which should be your first or second set. If you don't have a partner, train as close to muscle failure as possible, and perform a dropset on your last set of each exercise."},
-          ],
+          "video": "https://www.youtube.com/embed/xK9zpXvjFUg"
         },
       }
     ]
@@ -57,26 +52,24 @@ var json = [
   {
     "name": "Shoulders",
     "description": "Because the front delts get so much work on chest day, and the middle delts take the brunt of the weight on overhead pressing moves, it's not uncommon for the rear delts to be the smallest—and weakest—of the three. In this workout, you'll do them before the other two, while you have a bit more energy in the tank. Feel free to rearrange the order of the single-joint moves based on your weaknesses, though. If you judge your delts to be fairly balanced, simply rotate the order of the single-joint movements from one workout to the next.",
-
+    "notes": [
+        {"note": "These workouts don't include warm-up sets. Do as many as you need, but never take warm-ups to muscle failure."},
+        {"note":"Choose a weight that allows you to reach muscle failure by the target-rep listed"},
+        {"note":"If you have a spotter, do a few forced reps on your heaviest sets of overhead presses. If you don't have a partner, do a dropset on your last set of each exercise, reducing the weight by about 25 percent when you reach muscle failure and continuing on to a second point of muscle failure."}
+    ],
     "exercises": [
       {"exercise": {
           "names": "Dumbbell Shoulder Press",
           "sets": "4 sets x 6-8 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-12.gif",
-          "video": "https://www.youtube.com/embed/qEwKCR5JCog",
-          "notes": [
-              {"note": "These workouts don't include warm-up sets. Do as many as you need, but never take warm-ups to muscle failure."},
-              ],
+          "video": "https://www.youtube.com/embed/qEwKCR5JCog"
         }
       },
       {"exercise": {
           "names": "Upright Barbell Row",
           "sets": "3 sets x 8-10 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-12.gif",
-          "video": "https://www.youtube.com/embed/amCU-ziHITM",
-          "notes": [
-              {"note":"Choose a weight that allows you to reach muscle failure by the target-rep listed"},
-          ],
+          "video": "https://www.youtube.com/embed/amCU-ziHITM"
         }
       },
       {"exercise": {
@@ -90,10 +83,7 @@ var json = [
           "names": "Side Lateral Raise",
           "sets": "3 sets x 10-12 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-12.gif",
-          "video": "https://www.youtube.com/embed/3VcKaXpzqRo",
-          "notes": [
-              {"note":"If you have a spotter, do a few forced reps on your heaviest sets of overhead presses. If you don't have a partner, do a dropset on your last set of each exercise, reducing the weight by about 25 percent when you reach muscle failure and continuing on to a second point of muscle failure."},
-          ],
+          "video": "https://www.youtube.com/embed/3VcKaXpzqRo"
         }
       },
       {"exercise": {
@@ -109,7 +99,12 @@ var json = [
   {
     "name": "Back",
     "description": "In most cases the back is an afterthought for many beginners. The back can't be seen when you look in the mirror, and most people have a hard time contracting the back when they first start working out, so the majority of beginners do a few half-hearted sets of pull-downs and low cable rows and move on to something else.",
-
+    "notes": [
+        {"note": "Use a thumb press grip, where you use an over hand grip with your 4 fingers around the bar and press your thumbs on the bar. This will lead into the next pointer."},
+        {"note":"Use enough pressure on the bar to hold, but don't squeeze too hard on the bar or you will engage the forearms and biceps too much. The thumb press will help with this."},
+        {"note":"Holding onto the bar or weight, drive you elbows as far back as possible trying to pinch your shoulder blades together."},
+        {"note": "Feel your muscles contract and leave your ego at the door. Too many beginners use way to much weight and never feel the muscle working."}
+    ],
     "exercises": [
       {"exercise": {
           "names": "Bent Over Barbell Row",
@@ -122,31 +117,21 @@ var json = [
           "names": "Seated Cable Rows",
           "sets": "3 sets x 10-12 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-4.gif",
-          "video": "https://www.youtube.com/embed/xQNrFHEMhI4",
-          "notes": [
-              {"note": "Use a thumb press grip, where you use an over hand grip with your 4 fingers around the bar and press your thumbs on the bar. This will lead into the next pointer."},
-              {"note":"Use enough pressure on the bar to hold, but don't squeeze too hard on the bar or you will engage the forearms and biceps too much. The thumb press will help with this."},
-          ],
+          "video": "https://www.youtube.com/embed/xQNrFHEMhI4"
         }
       },
       {"exercise": {
           "names": "V-Bar Pulldown",
           "sets": "3 sets x 8-10 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-3.gif",
-          "video": "https://www.youtube.com/embed/-HA56qLRK84",
-          "notes": [
-              {"note":"Holding onto the bar or weight, drive you elbows as far back as possible trying to pinch your shoulder blades together."},
-          ],
+          "video": "https://www.youtube.com/embed/-HA56qLRK84"
         }
       },
       {"exercise": {
           "names": "Barbell Shrug",
           "sets": "3 sets x 6-8 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-11.gif",
-          "video": "https://www.youtube.com/embed/NAqCVe2mwzM",
-          "notes": [
-              {"note": "Feel your muscles contract and leave your ego at the door. Too many beginners use way to much weight and never feel the muscle working."},
-          ],
+          "video": "https://www.youtube.com/embed/NAqCVe2mwzM"
         }
       },
       {"exercise": {
@@ -164,16 +149,17 @@ var json = [
   {
     "name": "Legs",
     "description": "As you can see from the anatomy info above, there are a lot of muscles that make up the legs. The legs are used daily for basic activities like walking, standing, going upstairs, or just getting up from a chair. What does this mean to you as a trainer? If you are looking to put size on your legs, half ass workouts will not do anything; your legs are accustomed to working all day. Only dedication to intense hard work in the gym will break down the dense thick muscle fibers of the legs and their only choice will be to get bigger and stronger. You need to force your legs to grow. Your legs will resist you. They will burn, shake and burn some more while you work out, but you need to push through and want more, as this is the only way to make your legs grow.",
-
+    "notes": [
+        {"note": "With the isolation exercises I like to use a moderate (8-12) to high rep range (15-20). This helps pump up the muscle to deliver more nutrients and break up the fascia tissue for more growth."},
+        {"note":"All exercises should be performed in perfect form because bad form or habits you start now will follow you and will lead to lack of progress—or worse, an injury in the future."},
+        {"note":"Now that you understand which muscles make up your legs, their function, location, how to contract your back and the rep range needed to stimulate it, let's give you some workouts to help you build your legs."}
+    ],
     "exercises": [
       {"exercise": {
           "names": "Barbell Squat",
           "sets": "4 sets x 6-8 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-7.gif",
-          "video": "https://www.youtube.com/embed/SW_C1A-rejs",
-          "notes": [
-              {"note": "With the isolation exercises I like to use a moderate (8-12) to high rep range (15-20). This helps pump up the muscle to deliver more nutrients and break up the fascia tissue for more growth."},
-          ],
+          "video": "https://www.youtube.com/embed/SW_C1A-rejs"
         }
       },
       {"exercise": {
@@ -187,10 +173,7 @@ var json = [
           "names": "Leg Press",
           "sets": "3 sets x 8-10 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-7.gif",
-          "video": "https://www.youtube.com/embed/IZxyjW7MPJQ",
-          "notes": [
-              {"note":"All exercises should be performed in perfect form because bad form or habits you start now will follow you and will lead to lack of progress—or worse, an injury in the future."},
-          ],
+          "video": "https://www.youtube.com/embed/IZxyjW7MPJQ"
         }
       },
       {"exercise": {
@@ -204,10 +187,7 @@ var json = [
           "names": "Leg Extensions",
           "sets": "3 sets x 20 reps",
           "image": "https://artifacts.bbcomcdn.com/exercises-app/1.2.1/img/guide-7.gif",
-          "video": "https://www.youtube.com/embed/YyvSfVjQeL0",
-          "notes": [
-              {"note":"Now that you understand which muscles make up your legs, their function, location, how to contract your back and the rep range needed to stimulate it, let's give you some workouts to help you build your legs."},
-          ],
+          "video": "https://www.youtube.com/embed/YyvSfVjQeL0"
         }
       },
       {"exercise": {
@@ -265,18 +245,17 @@ var json = [
   {
       "name": "Circuit Training",
       "description": "Circuit training is an interval-based workout program. You complete several different exercises in rapid succession, with short rests in between. Generally, you'll perform each exercise at a high intensity for a set period of time or set number of reps.",
-
+      "notes": [
+          {"note": "Circuit training is simply a workout based around a set number of stations that you repeat until your time runs out."},
+          {"note":"Knowing how much time you have can help you determine how many circuits you'll need to complete and how hard you'll need to work."},
+          {"note":"The shorter the workout, the harder you should be pushing. Anywhere from 10 to 45 minutes is ideal for circuit training."}
+      ],
       "exercises": [
         {"exercise": {
             "names": "Getting Started",
             "sets": "Step 1: Select your time limit. \nStep 2: Pick an upper-body exercise. \n Step 3: Pick a lower-body exercise.\nStep 4: Pick a compound exercise. \nStep 5: Choose a sprint for 1 minute. \nStep 6: Rest for 1 minute.",
             "image": "https://www.builtlean.com/wp-content/uploads/2011/09/full-body-workout.jpg",
-            "video": "https://www.youtube.com/embed/io-G_0qRTL4",
-            "notes": [
-                {"note": "Circuit training is simply a workout based around a set number of stations that you repeat until your time runs out."},
-                {"note":"Knowing how much time you have can help you determine how many circuits you'll need to complete and how hard you'll need to work."},
-                {"note":"The shorter the workout, the harder you should be pushing. Anywhere from 10 to 45 minutes is ideal for circuit training."}
-            ],
+            "video": "https://www.youtube.com/embed/io-G_0qRTL4"
           },
         }
       ]
@@ -285,19 +264,18 @@ var json = [
   {
       "name": "Long Distance Run",
       "description": "Good running technique can help reduce your risk of injury and make your runs feel less tiring and more enjoyable.",
-
+      "notes": [
+          {"note": "Look straight ahead of you, about 30 to 40 metres out in front, and avoid looking down at your feet."},
+          {"note":"Your shoulders should be back and down. Keep them relaxed and avoid tensing them. Don't hunch over as this restricts breathing, allowing less oxygen to get to the muscles."},
+          {"note":"Your hands should be relaxed, but don't let them flop. Tight hands can cause tension all the way up to the back and shoulders."},
+  	{"note":"Whether you breathe through your nose or mouth, try to breathe deeply and rhythmically. Avoid shallow and quick breaths. Try to aim for one breath for every two strides, but don't be afraid to try longer breathing."}
+      ],
       "exercises": [
         {"exercise": {
-            "names": "Getting Started",
+            "names": "Before you Start",
             "sets": "You need to be properly warmed up. \nTHIS IS VERY IMPORTANT: don’t do too much too soon or you WILL hurt yourself. \nSet an ambitious, but realistic goal. \nRun!",
             "image": "http://www.fluidtechfitness.com/exer_cycle_muscle.jpg",
-            "video": "https://www.youtube.com/embed/5umbf4ps0GQ",
-            "notes": [
-                {"note": "Look straight ahead of you, about 30 to 40 metres out in front, and avoid looking down at your feet."},
-                {"note":"Your shoulders should be back and down. Keep them relaxed and avoid tensing them. Don't hunch over as this restricts breathing, allowing less oxygen to get to the muscles."},
-                {"note":"Your hands should be relaxed, but don't let them flop. Tight hands can cause tension all the way up to the back and shoulders."},
-          {"note":"Whether you breathe through your nose or mouth, try to breathe deeply and rhythmically. Avoid shallow and quick breaths. Try to aim for one breath for every two strides, but don't be afraid to try longer breathing."}
-            ],
+            "video": "https://www.youtube.com/embed/5umbf4ps0GQ"
           },
         }
       ]
@@ -306,18 +284,17 @@ var json = [
   {
       "name": "Interval Training",
       "description": "Interval training is when you alternate periods of moderate- to high-intensity work with periods of either active or passive rest. Unlike circuit training, interval training has less to do with what you're doing and, instead, is mostly about the intensity of what you're doing.",
-
+      "notes": [
+          {"note": "Choose strategically between active and passive rest: If you're doing a particularly difficult move you'll probably need to gulp some water and catch your breath during the rest interval."},
+          {"note":"A good week would be maybe two days of circuit training at a relatively moderate intensity, and two or three days of interval training at a moderate to high intensity."},
+          {"note":"If you do too much high-intensity training it could cause overtraining, which can cause adrenal fatigue and disrupt the hormone balance in your body."}
+      ],
       "exercises": [
         {"exercise": {
             "names": "Getting Started",
             "sets": "Try a 20 to 30-minute routine that mixes and matches these great interval exercise options. Start with a 5-minute warm-up and move into short high-intensity intervals. Each interval can last from 30 seconds to two minutes, with a minute of easy walking between reps. Go for about 20 minutes then cool down. It's a quick and effective workout.",
             "image": "https://www.builtlean.com/wp-content/uploads/2011/09/full-body-workout.jpg",
-            "video": "https://www.youtube.com/embed/DuYmoD8IQgM",
-            "notes": [
-                {"note": "Choose strategically between active and passive rest: If you're doing a particularly difficult move you'll probably need to gulp some water and catch your breath during the rest interval."},
-                {"note":"A good week would be maybe two days of circuit training at a relatively moderate intensity, and two or three days of interval training at a moderate to high intensity."},
-                {"note":"If you do too much high-intensity training it could cause overtraining, which can cause adrenal fatigue and disrupt the hormone balance in your body."}
-            ],
+            "video": "https://www.youtube.com/embed/DuYmoD8IQgM"
           },
         }
       ]
